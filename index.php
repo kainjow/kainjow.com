e0747dc1cf150e05e04786a984a05a8e5bdafe18
Kevin Wojniak <kainjow@kainjow.com>
2015-09-09 23:52:58 -0700

    Group projects by category

--- index.php
+++ index.php
@@ -1,94 +1,102 @@
 <?php
 
 $projects = array(
-	array(
-		'name' => 'BootChamp',
-		'version' => '1.7',
-		'date' => mktime(0, 0, 0, 8, 15, 2015),
-		'summary' => 'Quickly boot into Windows',
-		'link' => '/downloads/BootChamp.zip',
-		'link_label' => 'download for 10.6+ (64-bit only)',
-		'image' => 'images/bootchamp_icon.png',
+	'Apps' => array(
+		array(
+			'name' => 'BootChamp',
+			'version' => '1.7',
+			'date' => mktime(0, 0, 0, 8, 15, 2015),
+			'summary' => 'Quickly boot into Windows',
+			'link' => '/downloads/BootChamp.zip',
+			'link_label' => 'download for 10.6+ (64-bit only)',
+			'image' => 'images/bootchamp_icon.png',
+		),
+		array(
+			'name' => 'Semulov',
+			'version' => '2.1.1',
+			'date' => mktime(0, 0, 0, 9, 9, 2015),
+			'summary' => 'Eject volumes from the menu bar',
+			'link' => '/downloads/Semulov.zip',
+			'link_label' => 'download for 10.7+',
+			'image' => 'images/semulov_icon.png',
+		),
+		array(
+			'name' => 'CrashCrier',
+			'version' => '1.0.1',
+			'date' => mktime(0, 0, 0, 7, 26, 2014),
+			'summary' => 'Be notified when a process crashes',
+			'link' => '/downloads/CrashCrier.zip',
+			'link_label' => 'download for 10.8+',
+			'image' => 'images/crashcrier_icon.png',
+		),
+		array(
+			'name' => 'DropJPG',
+			'version' => '5.0',
+			'date' => mktime(0, 0, 0, 1, 24, 2015),
+			'summary' => 'Batch convert images to jpg',
+			'link' => '/downloads/DropJPG.zip',
+			'link_label' => 'download for 10.8+',
+			'image' => 'images/dropjpg_icon.png',
+		),
+		array(
+			'name' => 'BBEncoder',
+			'version' => '1.1',
+			'date' => mktime(0, 0, 0, 2, 28, 2012),
+			'summary' => 'Convert rich text to BBCode',
+			'link' => '/downloads/BBEncoder.zip',
+			'link_label' => 'download for 10.5+',
+			'image' => 'images/bbencoder_icon.png',
+		),
 	),
-	array(
-		'name' => 'Semulov',
-		'version' => '2.1.1',
-		'date' => mktime(0, 0, 0, 9, 9, 2015),
-		'summary' => 'Eject volumes from the menu bar',
-		'link' => '/downloads/Semulov.zip',
-		'link_label' => 'download for 10.7+',
-		'image' => 'images/semulov_icon.png',
+	'Screensavers' => array(
+		array(
+			'name' => 'Spread',
+			'version' => '1.0.1',
+			'date' => mktime(0, 0, 0, 2, 28, 2012),
+			'summary' => 'Colorful screensaver',
+			'link' => '/downloads/Spread.zip',
+			'link_label' => 'download for 10.6+',
+			'image' => 'images/spreadIconShot.png',
+		),
+		array(
+			'name' => 'Pellucid',
+			'version' => '1.0.1',
+			'date' => mktime(0, 0, 0, 10, 18, 2012),
+			'summary' => 'Dimmed desktop screensaver',
+			'link' => '/downloads/Pellucid.saver.zip',
+			'link_label' => 'download for 10.6+',
+			'image' => 'images/screensaver_icon.png',
+		),
+		array(
+			'name' => 'FoolSaver',
+			'version' => '1.2.1',
+			'date' => mktime(0, 0, 0, 8, 20, 2014),
+			'summary' => 'Mimic Windows logo screensaver',
+			'link' => '/downloads/FoolSaver.zip',
+			'link_label' => 'download for 10.7+',
+			'image' => 'images/screensaver_icon.png',
+		),
 	),
-	array(
-		'name' => 'DropJPG',
-		'version' => '5.0',
-		'date' => mktime(0, 0, 0, 1, 24, 2015),
-		'summary' => 'Batch convert images to jpg',
-		'link' => '/downloads/DropJPG.zip',
-		'link_label' => 'download for 10.8+',
-		'image' => 'images/dropjpg_icon.png',
-	),
-	array(
-		'name' => 'CrashCrier',
-		'version' => '1.0.1',
-		'date' => mktime(0, 0, 0, 7, 26, 2014),
-		'summary' => 'Be notified when a process crashes',
-		'link' => '/downloads/CrashCrier.zip',
-		'link_label' => 'download for 10.8+',
-		'image' => 'images/crashcrier_icon.png',
-	),
-	array(
-		'name' => 'ScriptQL',
-		'version' => '1.1',
-		'date' => mktime(0, 0, 0, 9, 6, 2012),
-		'summary' => 'AppleScript Quick Look plugin',
-		'link' => '/downloads/ScriptQL_qlgenerator.zip',
-		'link_label' => 'download for 10.6+',
-		'image' => 'images/scriptqlIconShot.png',
-		'is_screenshot' => true,
-	),
-	array(
-		'name' => 'BBEncoder',
-		'version' => '1.1',
-		'date' => mktime(0, 0, 0, 2, 28, 2012),
-		'summary' => 'Convert rich text to BBCode',
-		'link' => '/downloads/BBEncoder.zip',
-		'link_label' => 'download for 10.5+',
-		'image' => 'images/bbencoder_icon.png',
-	),
-	array(
-		'name' => 'Spread',
-		'version' => '1.0.1',
-		'date' => mktime(0, 0, 0, 2, 28, 2012),
-		'summary' => 'Colorful screensaver',
-		'link' => '/downloads/Spread.zip',
-		'link_label' => 'download for 10.6+',
-		'image' => 'images/spreadIconShot.png',
-	),
-	array(
-		'name' => 'Pellucid',
-		'version' => '1.0.1',
-		'date' => mktime(0, 0, 0, 10, 18, 2012),
-		'summary' => 'Dimmed desktop screensaver',
-		'link' => '/downloads/Pellucid.saver.zip',
-		'link_label' => 'download for 10.6+',
-		'image' => 'images/screensaver_icon.png',
-	),
-	array(
-		'name' => 'FoolSaver',
-		'version' => '1.2.1',
-		'date' => mktime(0, 0, 0, 8, 20, 2014),
-		'summary' => 'Mimic Windows logo screensaver',
-		'link' => '/downloads/FoolSaver.zip',
-		'link_label' => 'download for 10.7+',
-		'image' => 'images/screensaver_icon.png',
+	'Miscellaneous' => array(
+		array(
+			'name' => 'ScriptQL',
+			'version' => '1.1',
+			'date' => mktime(0, 0, 0, 9, 6, 2012),
+			'summary' => 'AppleScript Quick Look plugin',
+			'link' => '/downloads/ScriptQL_qlgenerator.zip',
+			'link_label' => 'download for 10.6+',
+			'image' => 'images/scriptqlIconShot.png',
+			'is_screenshot' => true,
+		)
 	),
 );
 
 $now = time();
 $new_days = 86400 * 15;
-foreach ($projects as &$proj) {
-	$proj['new'] = ($now - $proj['date']) <= $new_days;
+foreach ($projects as $groupName => &$subProjects) {
+	foreach ($subProjects as &$proj) {
+		$proj['new'] = ($now - $proj['date']) <= $new_days;
+	}
 }
 unset($proj); // http://stackoverflow.com/a/31043429/412179
 
@@ -117,8 +125,16 @@ unset($proj); // http://stackoverflow.com/a/31043429/412179
 	<p>Visit my <a href="http://kainjow.tumblr.com">blog</a> or follow me on <a href="https://twitter.com/kainjow">Twitter</a> for the latest updates. You can also <a href="email.php">email</a> me.</p>
 	</div>
 	
-	<div class="row">
-		<?php foreach ($projects as $proj): ?>
+	<?php foreach ($projects as $groupName => $groupProjects): ?>
+	<div class="panel panel-default">
+	  <div class="panel-heading">
+	    <h3 class="panel-title"><?= $groupName ?></h3>
+	  </div>
+
+	<div class="panel-body">
+	<div class="row">	
+	
+		<?php foreach ($groupProjects as $proj): ?>
 		<div class="col-sm-4 col-xs-6 margintop">
 		<img class="img-responsive center-block producticon" src="<?= $proj['image']; ?>" alt="<?= $proj['name']; ?>">
 		<p class="text-center productinfo">
@@ -129,7 +145,10 @@ unset($proj); // http://stackoverflow.com/a/31043429/412179
 		</p>
 		</div>
 		<?php endforeach ?>
+    </div>	
 	</div>
+</div>
+	<?php endforeach ?>
 	
 	<div>
 	<p class="footer text-center margintop">
